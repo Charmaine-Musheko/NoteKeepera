f10bb538e71128e90daee8f4eabc447942b407b8
Charmaine <chamainemush@gmail.com>
2020-07-19 17:22:30 +0200

    l.,

--- app/src/main/java/com/example/notekeeper/NotekeeperDatabaseContract.java
+++ app/src/main/java/com/example/notekeeper/NotekeeperDatabaseContract.java
@@ -10,7 +10,8 @@ public final class NotekeeperDatabaseContract {
         public static final String COLUMN_COURSE_TITLE = "course_title";
         //COURSE_TABLE course_info (course_id, course_title)
         public static final String SQL_CREATE_TABLE =
-                "CREATE TABLE " + TABLE_NAME + " (" + " INTEGER PRIMARY KEY, " +
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY, " +
                 COLUMN_COURSE_ID + " TEXT UNIQUE NOT NULL, " +
                 COLUMN_COURSE_TITLE + " TEXT NOT NULL)";
     }
@@ -18,12 +19,14 @@ public final class NotekeeperDatabaseContract {
         public  static final String TABLE_NAME = "note_info";
         public static final String COLUMN_NOTE_TITLE = "note_title";
         public static  final String COLUMN_NOTE_TEXT = "note_text";
-        public static  final String COLUMN_COURSE_ID = "note_id";
+        public static  final String COLUMN_COURSE_ID = "course_id";
 
         //COURSE_TABLE course_info (course_id, course_title)
         public static final String SQL_CREATE_TABLE =
-                "CREATE TABLE " + TABLE_NAME + " (" + " INTEGER PRIMARY KEY, " +
-                        COLUMN_NOTE_TITLE + " TEXT NOT NULL, " + COLUMN_NOTE_TEXT + " TEXT, " +
+                "CREATE TABLE " + TABLE_NAME + " (" +  _ID +
+                        " INTEGER PRIMARY KEY, " +
+                        COLUMN_NOTE_TITLE + " TEXT NOT NULL, " +
+                        COLUMN_NOTE_TEXT + " TEXT, " +
                         COLUMN_COURSE_ID + " TEXT NOT NULL)";
 
     }
